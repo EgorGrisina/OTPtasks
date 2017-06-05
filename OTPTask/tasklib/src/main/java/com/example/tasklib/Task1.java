244e301eb5dbc6fb6a1fe88f278cb2faf5465884
Egor Gritsina <egor@egor-debian>
2017-06-05 23:00:02 +0300

    Init tasks lib

---
+++ OTPTask/tasklib/src/main/java/com/example/tasklib/Task1.java
@@ -0,0 +1,13 @@
+package com.example.tasklib;
+
+/**
+ * Created by Egor Gritsina on 6/5/17.
+ */
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
